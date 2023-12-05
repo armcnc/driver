7550359987c4b6cc1766b3822154615559db0fbb
GeekROS <15300555770>
2023-12-05 18:19:04 +0800

    1.0.0

--- armcnc_driver.h
+++ armcnc_driver.h
@@ -2,4 +2,10 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
 #include <wiringPi.h>
