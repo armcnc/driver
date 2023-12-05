47ad6fc7af5e3905e343fd59ed56b2d3ed11bb38
GeekROS <15300555770>
2023-12-05 19:03:52 +0800

    1.0.0

--- armcnc_driver.c
+++ armcnc_driver.c
@@ -5,11 +5,11 @@
  ******************************************************************************
  */
 
-#include "armcnc_driver.h"
 #include "rtapi.h"
 #include "rtapi_app.h"
 #include "rtapi_math.h"
 #include "hal.h"
+#include "armcnc_driver.h"
 
 #define MAX_PINS 40
 #define MAX_INI_LINE_LENGTH 255
--- armcnc_driver.h
+++ armcnc_driver.h
@@ -1,3 +1,10 @@
+/**
+ ******************************************************************************
+ * @file    armcnc_driver.h
+ * @author  ARMCNC site:www.armcnc.net github:armcnc.github.io
+ ******************************************************************************
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
