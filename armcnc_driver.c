84309aa2c15e287a11c55014050cbccd6551f520
GeekROS <15300555770>
2023-12-06 14:38:52 +0800

    1.0.0

--- armcnc_driver.c
+++ armcnc_driver.c
@@ -9,6 +9,8 @@
 #include "./lib/wiringPi/piHiPri_c.h"
 #include "./lib/wiringPi/wiringPi.h"
 #include "./lib/wiringPi/wiringPi_c.h"
+#include "./lib/wiringPi/softPwm.h"
+#include "./lib/wiringPi/softPwm_c.h"
 
 #include "rtapi.h"
 #include "rtapi_app.h"
