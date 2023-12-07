3f029f24b4ca84d547ce72b750230504b25bb66b
GeekROS <15300555770>
2023-12-07 10:41:06 +0800

    1.0.0

--- armcnc_driver.c
+++ armcnc_driver.c
@@ -234,11 +234,6 @@ int rtapi_app_main(void)
         return -1;
     }
 
-    if (wiringPiSetup() == -1){
-        rtapi_print_msg(RTAPI_MSG_ERR, "[error]: wiringPiSetup\n");
-        return -1;
-    }
-
     int32_t mem_fd = open("/dev/mem", O_RDWR|O_SYNC);
     if (mem_fd < 0) {
         rtapi_print_msg(RTAPI_MSG_ERR, "[error]: /dev/mem\n");
@@ -247,6 +242,11 @@ int rtapi_app_main(void)
 
     if (system("sudo chmod 777 /dev/mem") != 0) {
         rtapi_print_msg(RTAPI_MSG_ERR, "[error]: chmod /dev/mem\n");
+        return -1;
+    }
+
+    if (wiringPiSetup() == -1){
+        rtapi_print_msg(RTAPI_MSG_ERR, "[error]: wiringPiSetup\n");
         return -1;
     }
 
