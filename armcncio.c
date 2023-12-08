13a7e9be870a227f4a29a773b870322bd57a9965
GeekROS <15300555770>
2023-12-08 13:03:53 +0800

    1.0.0

--- armcncio.c
+++ armcncio.c
@@ -13,16 +13,16 @@ MODULE_DESCRIPTION("Driver for ARMCNC");
 MODULE_LICENSE("GPL");
 #endif
 
-static char *ins = "";
+static char *in_pins = "";
 #ifdef RTAPI
-RTAPI_MP_STRING(ins, "channels control type, comma separated");
+RTAPI_MP_STRING(in_pins, "channels control type, comma separated");
 #endif
 static int in_pins_array[GPIO_MAX_COUNT];
 static int in_pins_count = 0;
 
-static char *outs = "";
+static char *out_pins = "";
 #ifdef RTAPI
-RTAPI_MP_STRING(outs, "channels control type, comma separated");
+RTAPI_MP_STRING(out_pins, "channels control type, comma separated");
 #endif
 static int out_pins_array[GPIO_MAX_COUNT];
 static int out_pins_count = 0;
@@ -43,7 +43,7 @@ static int32_t malloc_and_export(const char *component_name, int32_t component_i
         return -1;
     }
 
-    char *in_pins_token = strtok(ins, ",");
+    char *in_pins_token = strtok(in_pins, ",");
     while (in_pins_token != NULL)
     {
         in_pins_array[in_pins_count] = atoi(in_pins_token);
@@ -51,7 +51,7 @@ static int32_t malloc_and_export(const char *component_name, int32_t component_i
         in_pins_token = strtok(NULL, ",");
     }
 
-    for (int in_pins_i = 0; in_pins_i < in_pins_count; in_pins_i++) {
+    for (int in_pins_i = 0; in_pins_i < in_pins_count; in_pins_i++)
     {
         retval = hal_pin_bit_newf(HAL_IN, &gpio_hal[in_pins_array[in_pins_i]], component_id, "%s.gpio.pin%d-%s", component_name, in_pins_array[in_pins_i], "in");
         retval += hal_pin_bit_newf(HAL_IN, &gpio_hal_not[in_pins_array[in_pins_i]], component_id, "%s.gpio.pin%d-%s-not", component_name, in_pins_array[in_pins_i], "in");
@@ -61,7 +61,7 @@ static int32_t malloc_and_export(const char *component_name, int32_t component_i
         }
     }
 
-    char *out_pins_token = strtok(outs, ",");
+    char *out_pins_token = strtok(out_pins, ",");
     while (out_pins_token != NULL)
     {
         out_pins_array[in_pins_count] = atoi(out_pins_token);
@@ -69,7 +69,7 @@ static int32_t malloc_and_export(const char *component_name, int32_t component_i
         out_pins_token = strtok(NULL, ",");
     }
 
-    for (int out_pins_i = 0; out_pins_i < out_pins_count; out_pins_i++) {
+    for (int out_pins_i = 0; out_pins_i < out_pins_count; out_pins_i++)
     {
         retval = hal_pin_bit_newf(HAL_OUT, &gpio_hal[out_pins_array[out_pins_i]], component_id, "%s.gpio.pin%d-%s", component_name, out_pins_array[out_pins_i], "out");
         retval += hal_pin_bit_newf(HAL_OUT, &gpio_hal_not[out_pins_array[out_pins_i]], component_id, "%s.gpio.pin%d-%s-not", component_name, out_pins_array[out_pins_i], "out");
