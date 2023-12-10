69c134cf6dc45fc152dda05ecd3c614f6e150e91
GeekROS <15300555770>
2023-12-10 13:31:16 +0800

    1.0.0

--- armcncio.c
+++ armcncio.c
@@ -160,7 +160,7 @@ static int32_t drives_init(const char *component_name, int32_t component_id)
         }
 
         *gpio_hal_out[out_pins_array[out_pins_i]] = digitalRead(out_pins_array[out_pins_i]) == HIGH ? 1 : 0;
-        *gpio_hal_out_not[out_pins_array[out_pins_i]] = *gpio_hal_in[out_pins_array[out_pins_i]] ? 0 : 1;
+        *gpio_hal_out_not[out_pins_array[out_pins_i]] = *gpio_hal_out[out_pins_array[out_pins_i]] ? 0 : 1;
         gpio_hal_out_prev[out_pins_array[out_pins_i]] = *gpio_hal_out[out_pins_array[out_pins_i]];
         gpio_hal_out_not_prev[out_pins_array[out_pins_i]] = *gpio_hal_out_not[out_pins_array[out_pins_i]];
     }
