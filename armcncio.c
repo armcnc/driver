ca431ffcc05f326a1f387f1a2e4b87d0b64f3fc0
GeekROS <15300555770>
2023-12-10 14:41:29 +0800

    1.0.0

--- armcncio.c
+++ armcncio.c
@@ -159,6 +159,9 @@ static int32_t drives_init(const char *component_name, int32_t component_id)
             return -1;
         }
 
+        // disable pin pull up/down
+        pullUpDnControl(out_pins_array[out_pins_i], PUD_OFF);
+
         *gpio_hal_out[out_pins_array[out_pins_i]] = digitalRead(out_pins_array[out_pins_i]) == HIGH ? 1 : 0;
         *gpio_hal_out_not[out_pins_array[out_pins_i]] = *gpio_hal_out[out_pins_array[out_pins_i]] ? 0 : 1;
         gpio_hal_out_prev[out_pins_array[out_pins_i]] = *gpio_hal_out[out_pins_array[out_pins_i]];
