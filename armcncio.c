0668760054f09ca93c4aa6ed0a4c353411d6b654
GeekROS <15300555770>
2023-12-12 15:03:08 +0800

    1.0.0

--- armcncio.c
+++ armcncio.c
@@ -254,8 +254,7 @@ static int32_t drives_init(const char *component_name, int32_t component_id)
 
 static void gpio_read(void *arg, long period)
 {
-    static uint32_t port, pin;
-    static int port_state;
+    static uint32_t port, pin, port_state;
 
     if (!gpio_in_cnt) return;
 
@@ -263,11 +262,11 @@ static void gpio_read(void *arg, long period)
     {
         if (!gpio_in_mask[port]) continue;
 
+        port_state = (uint32_t)digitalRead((int)port);
+
         for (pin = gpio_pins_cnt[port]; pin--;)
         {
             if (!(gpio_in_mask[port] & pin_msk[pin])) continue;
-
-            port_state = digitalRead((int)pin);
 
             if (port_state & pin_msk[pin]) {
                 *gpio_hal[port][pin] = 1;
