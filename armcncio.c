c4cdb725b54ff30e9b23591eb10d8e1a804acc9f
GeekROS <15300555770>
2023-12-13 16:56:21 +0800

    1.0.0

--- armcncio.c
+++ armcncio.c
@@ -390,28 +390,13 @@ static void gpio_read(void *arg, long period)
     {
         if (!in_pins_count || !out_pins_count) continue;
         
-        if (isInArray(in_pins_array, in_pins_count, pins_i))
+        if (digitalRead(in_pins_array[pins_i]) == HIGH)
         {
-            if (digitalRead(in_pins_array[pins_i]) == HIGH)
-            {
-                *gpio_hal_in[in_pins_array[pins_i]] = 1;
-                *gpio_hal_in_not[in_pins_array[pins_i]] = 0;
-            }else{
-                *gpio_hal_in[in_pins_array[pins_i]] = 0;
-                *gpio_hal_in_not[in_pins_array[pins_i]] = 1;
-            }
-        }
-
-        if (isInArray(out_pins_array, out_pins_count, pins_i))
-        {
-            if (digitalRead(in_pins_array[pins_i]) == HIGH)
-            {
-                *gpio_hal_in[in_pins_array[pins_i]] = 1;
-                *gpio_hal_in_not[in_pins_array[pins_i]] = 0;
-            }else{
-                *gpio_hal_in[in_pins_array[pins_i]] = 0;
-                *gpio_hal_in_not[in_pins_array[pins_i]] = 1;
-            }
+            *gpio_hal_in[in_pins_array[pins_i]] = 1;
+            *gpio_hal_in_not[in_pins_array[pins_i]] = 0;
+        }else{
+            *gpio_hal_in[in_pins_array[pins_i]] = 0;
+            *gpio_hal_in_not[in_pins_array[pins_i]] = 1;
         }
     }
 }
