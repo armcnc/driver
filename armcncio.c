da59e0d1d8d01ccc44b11a3b34713ea5be8e50e4
GeekROS <15300555770>
2023-12-17 14:35:47 +0800

    1.0.0

--- armcncio.c
+++ armcncio.c
@@ -359,9 +359,9 @@ static void pwm_write(void *arg, long period)
 
         softPwmWrite((int)(*pwm_hal[ch].pwm_pin), pwm_cycle);
 
-        if (target_rpm > 0) digitalWrite((int)(*pwm_hal[ch].dir_pin), *pwm_hal[ch].dir_pin_not ? LOW : HIGH);
-        if (target_rpm == 0) digitalWrite((int)(*pwm_hal[ch].dir_pin), NOTD);
-        if (target_rpm < 0) digitalWrite((int)(*pwm_hal[ch].dir_pin), *pwm_hal[ch].dir_pin_not ? HIGH : LOW);
+        // if (target_rpm > 0) digitalWrite((int)(*pwm_hal[ch].dir_pin), *pwm_hal[ch].dir_pin_not ? LOW : HIGH);
+        // if (target_rpm == 0) digitalWrite((int)(*pwm_hal[ch].dir_pin), NOTD);
+        // if (target_rpm < 0) digitalWrite((int)(*pwm_hal[ch].dir_pin), *pwm_hal[ch].dir_pin_not ? HIGH : LOW);
     }
 }
 
