cb30b84d6eb18089b0303b043a91f150b32eedf3
GeekROS <15300555770>
2023-12-17 14:05:04 +0800

    1.0.0

--- armcncio.c
+++ armcncio.c
@@ -315,6 +315,7 @@ static void pwm_write(void *arg, long period)
         if(!pwm_hal_prev[ch].is_init)
         {
             pinMode((int)(*pwm_hal[ch].pwm_pin), OUTPUT);
+            pinMode((int)(*pwm_hal[ch].dir_pin), OUTPUT);
             pullUpDnControl((int)(*pwm_hal[ch].pwm_pin), PUD_OFF);
             softPwmCreate((int)(*pwm_hal[ch].pwm_pin), 0, 100);
             pwm_hal_prev[ch].is_init = 1;
@@ -349,13 +350,19 @@ static void pwm_write(void *arg, long period)
 
         int max_rpm = (int)(*pwm_hal[ch].dc_scale);
         int target_rpm = (int)(*pwm_hal[ch].dc_cmd);
-        int pwm_cycle = (target_rpm * 100) / max_rpm;
+        int pwm_cycle = abs(target_rpm * 100) / max_rpm;;
 
         if (*pwm_hal[ch].pwm_pin_not) {
             pwm_cycle = 100 - pwm_cycle;
         }
 
         softPwmWrite((int)(*pwm_hal[ch].pwm_pin), pwm_cycle);
+
+        if (target_rpm >= 0) {
+            digitalWrite((int)(*pwm_hal[ch].dir_pin), *pwm_hal[ch].dir_pin_not ? LOW : HIGH);
+        } else {
+            digitalWrite((int)(*pwm_hal[ch].dir_pin), *pwm_hal[ch].dir_pin_not ? HIGH : LOW);
+        }
     }
 }
 
