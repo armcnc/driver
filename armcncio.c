99f2ab13617e4a314617059f2a7c256756cb498a
GeekROS <15300555770>
2023-12-17 14:17:50 +0800

    1.0.0

--- armcncio.c
+++ armcncio.c
@@ -349,7 +349,7 @@ static void pwm_write(void *arg, long period)
 
         int max_rpm = (int)(*pwm_hal[ch].dc_scale);
         int target_rpm = (int)(*pwm_hal[ch].dc_cmd);
-        int pwm_cycle = abs(target_rpm * 100) / max_rpm;;
+        int pwm_cycle = abs(target_rpm * 100) / max_rpm;
 
         if (*pwm_hal[ch].pwm_pin_not) {
             pwm_cycle = 100 - pwm_cycle;
