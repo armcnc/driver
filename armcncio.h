63f8ca95203e07b246fb87a9b0e757762f925e29
GeekROS <15300555770>
2023-12-28 09:56:01 +0800

    1.0.0

--- armcncio.h
+++ armcncio.h
@@ -166,12 +166,11 @@ static int spindle_control(int ch)
 {
     if (!pwm_hal_prev[ch].is_init)
     {
-        // softPwmCreate((int)(*pwm_hal[ch].spindle_pin), 0, 100);
         pwm_hal_prev[ch].is_init = 1;
         return 1;
     }
 
-    // pwm_update_data(ch);
+    pwm_update_data(ch);
 
     if (!(*pwm_hal[ch].enable))
     {
